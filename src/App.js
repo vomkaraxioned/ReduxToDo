0799b080601fae5e463ce1d8669a74ae74ee54ee
vomkaraxioned <omkarv@axioned.com>
2022-12-26 18:37:54 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     dispatch = useDispatch(),
     [tabFilter, setTabFilter] = useState("active"),
     controlsHandler = { dispatch, edit, remove, complete,saveEdited };
+    document.body.classList.add("bg-[#8074c5]");
 
   return (
     <>
--- src/style/InputContainer.style.js
+++ src/style/InputContainer.style.js
@@ -3,13 +3,17 @@ const InputContainer = tw.div`
   flex
   bg-blue-900
   ${props=>props.basis === "60%"?"basis-9/12":"basis-2/11"}
-  // last:after:flex
-  // last:after:w-30
-  // last:after:h-30
-  // last:after:font-[FontAwesome]
-  // last:after:font-black
-  // last:after:bg-red-900
-  // last:after:content-['hello']
+  mr-5
+  last:mr-0
+  last:justify-center
+  last:after:flex
+  last:after:w-30
+  last:after:h-30
+  last:after:font-['FontAwesome']
+  last:after:font-black
+  last:after:bg-red-900
+  last:after:content-['\f002']
+  last:after:float-right
 `;
 
 export default InputContainer;
