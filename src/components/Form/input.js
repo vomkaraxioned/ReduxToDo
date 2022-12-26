5e7e5581b580b962cdec29fb7b8820ac0cde0617
vomkaraxioned <omkarv@axioned.com>
2022-12-26 19:27:32 +0530

    tailwind submit btn styling started

--- src/components/Form/input.js
+++ src/components/Form/input.js
@@ -7,7 +7,7 @@ const Input = ({ type, name, styleName, basis, placeholder, handler, index }) =>
 
   return (
     <InputContainer basis={basis} isBtn = {styleName === "btn btn-submit"?true:false}>
-      <input type={type} name={name} className={`flex-1 py-1 px-5 border-slate-700 focus:outline-blue-900 ${styleName === "btn btn-submit"?"text-none":"capitalize" }`} placeholder={placeholder} ref={reference} onChange={() => handler(reference.current.value)} />
+      <input type={type} name={name} className={`flex-1 py-1 px-5 border-slate-700 focus:outline-blue-900 ${styleName === "btn btn-submit"?"indent-[-9999px] fa-solid fa-house":"capitalize" }`} placeholder={placeholder} ref={reference} onChange={() => handler(reference.current.value)} />
     </InputContainer>
   );
 };
--- src/style/InputContainer.style.js
+++ src/style/InputContainer.style.js
@@ -1,19 +1,11 @@
 import tw from "tailwind-styled-components";
+
 const InputContainer = tw.div`
   flex
   bg-blue-900
   ${props=>props.basis === "60%"?"basis-9/12":"basis-2/11"}
   mr-5
   last:mr-0
-  last:justify-center
-  last:after:flex
-  last:after:w-30
-  last:after:h-30
-  last:after:font-['FontAwesome']
-  last:after:font-black
-  last:after:bg-red-900
-  last:after:content-['\f002']
-  last:after:float-right
 `;
 
 export default InputContainer;
