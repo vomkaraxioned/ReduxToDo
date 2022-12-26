51253040aa3ee764cf6d5af636bf5e8a520d22c0
vomkaraxioned <omkarv@axioned.com>
2022-12-26 12:53:15 +0530

    tailwind css changes done

--- src/style/InputContainer.style.js
+++ src/style/InputContainer.style.js
@@ -1,12 +1,15 @@
 import tw from "tailwind-styled-components";
-
 const InputContainer = tw.div`
   flex
+  bg-blue-900
   ${props=>props.basis === "60%"?"basis-9/12":"basis-2/11"}
-  last:before:font-['FontAwesome']
-  last:before:font-black
-  last:before:content-['\f0fe']
-
+  // last:after:flex
+  // last:after:w-30
+  // last:after:h-30
+  // last:after:font-[FontAwesome]
+  // last:after:font-black
+  // last:after:bg-red-900
+  // last:after:content-['hello']
 `;
 
 export default InputContainer;
