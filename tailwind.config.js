37169d21ac1324a843c326780733cc057752c550
vomkaraxioned <omkarv@axioned.com>
2022-12-28 09:26:00 +0530

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
       'tablet':'768px',
       'largePhone':'540px'
     },
-    extend: {},
+    extend: {
+      keyframes:{
+        scaleout:{
+           'from':{transform:'translateX(100%)'},
+            'to':{transform:'translateX(0)'}
+        }
+      }
+    },
   },
   plugins: [],
 }
